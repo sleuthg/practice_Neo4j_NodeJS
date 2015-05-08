9402503578ff04e09ec97a92df98f7f74d6faa15
sleuthg <sleuthg@gmail.com>
2015-05-08 12:18:56 -0700

    added ability to print test data, load some of the data to the database, and clear the database of the data loaded

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var path = require('path');
 var http = require('http');
 var fs = require('fs');
 var dbapi = require('./lib/dbapi.js');
+//var async = require("async");
 
 // Instantiate the Express.JS application
 var app = express();
@@ -25,13 +26,6 @@ var port = 7474; // <-- default Neo4j port
 // (auth set in neo4j-server.properties file in /conf)
 var httpUrlForTransaction = 'http://' + host + ':' + port + '/db/data/transaction/commit';
 
-var datafile = './data/learning_threads_001.json';
-
-// Read the test data
-// @TODO: Turn this into a function with the filename as a parameter
-var data = dbapi.loadFile(fs, datafile);
-//dbapi.printData(data);
-
 // Define a function which fires the cypher query.
 function runCypherQuery(query, params, callback) {
   request.post({
@@ -44,19 +38,9 @@ function runCypherQuery(query, params, callback) {
 
 }
 
-/**
- * Let’s fire some queries below.
- * */
-// runCypherQuery(
-//   'CREATE (somebody:Person { name: {name}, from: {company}, age: {age} }) RETURN somebody', {
-//     name: 'Ghuffran',
-//     company: 'Modulus',
-//     age: 44
-//   }, function (err, resp) {
-//     if (err) {
-//       console.log(err);
-//     } else {
-//       console.log(resp);
-//     }
-//   }
-// );
+// Clear the database of Stitches and Yarn
+dbapi.clearDB(runCypherQuery);
+
+var dataFile = './data/learning_threads_001.json';
+//dbapi.loadFile(fs,dataFile, dbapi.printData);
+dbapi.loadFile(fs,dataFile,dbapi.addDataToDB);
